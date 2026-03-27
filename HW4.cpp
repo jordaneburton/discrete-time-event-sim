eabe49d3eabf35cba84e512c53b0befd9cdb66fe
Jordan Burton <jordane.burton@gmail.com>
2026-03-27 11:48:07 -0500

    setup run_simulation() with initiating state variables

---
+++ HW4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+void run_simulation(const int MAX_PROCESSES) {
+    int clock = 0;
+    int processes_count = 0;
+
+    bool server_busy = false;
+    // Simulation code goes here
+}
+
+int main() {
+    const int PROCESS_LIMIT = 10000;
+    run_simulation(PROCESS_LIMIT);
+
+    return 0;
+}
