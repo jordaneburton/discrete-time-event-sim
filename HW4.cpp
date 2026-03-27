65dcc7412083570720a01f6393bfca4c05eed3f9
Jordan Burton <jordane.burton@gmail.com>
2026-03-27 11:51:52 -0500

    added Process structure and ready queue state var to run_simulation()

--- HW4.cpp
+++ HW4.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 #include <vector>
 
+struct Process {
+    /* data */
+    int id;
+    int arrival_time;
+    bool is_arriving;
+};
+
+
 void run_simulation(const int MAX_PROCESSES) {
     int clock = 0;
     int processes_count = 0;
 
     bool server_busy = false;
+    std::vector<Process> ready_queue;
     // Simulation code goes here
 }
 
