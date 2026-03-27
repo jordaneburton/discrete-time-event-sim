ad155eb08deab9b0a07dbb74814fd80226bd9a0d
Jordan Burton <jordane.burton@gmail.com>
2026-03-27 12:01:42 -0500

    created Simulation class and repurposed functions into class methods

--- HW4.cpp
+++ HW4.cpp
@@ -1,26 +1,49 @@
 #include <iostream>
 #include <vector>
 
-struct Process {
-    /* data */
-    int id;
-    int arrival_time;
-    bool is_arriving;
+
+class Simulation {
+    private:
+        int clock;
+        int processes_count;
+        const int MAX_PROCESSES;
+
+        bool server_busy;
+        std::vector<Process> ready_queue;
+        struct Process {
+            /* data */
+            int id;
+            int arrival_time;
+            bool is_arriving;
+        };    
+
+        void handle_arrival(Process *process) {
+            // Handle arrival event
+        }
+        
+        void handle_departure(Process *process) {
+            // Handle departure event
+        }
+
+    public:
+        Simulation(const int max_procs) : MAX_PROCESSES(max_procs) {
+            // Initialize simulation parameters
+            clock = 0;
+            processes_count = 0;
+            server_busy = false;
+        }
+
+        void run() {
+            // Run the simulation
+        }
 };
 
 
-void run_simulation(const int MAX_PROCESSES) {
-    int clock = 0;
-    int processes_count = 0;
-
-    bool server_busy = false;
-    std::vector<Process> ready_queue;
-    // Simulation code goes here
-}
 
 int main() {
     const int PROCESS_LIMIT = 10000;
-    run_simulation(PROCESS_LIMIT);
+    Simulation sim(PROCESS_LIMIT);
+    sim.run();
 
     return 0;
 }
